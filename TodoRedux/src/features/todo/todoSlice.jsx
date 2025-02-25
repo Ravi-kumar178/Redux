28a527a30d762f61f71cd5d74e25765c904d0174
Ravi Kumar <bhardwajravi2025@gmail.com>
2025-02-25 16:37:20 +0530

    redux

---
+++ TodoRedux/src/features/todo/todoSlice.jsx
@@ -0,0 +1,35 @@
+import { createSlice, nanoid } from "@reduxjs/toolkit";
+
+const initialState = {
+    todos: (() => {
+        const stored = localStorage.getItem("todos");
+        if (stored) {
+          const parsed = JSON.parse(stored);
+          return Array.isArray(parsed) ? parsed : [parsed];
+        }
+        return [];
+      })
+};
+
+export const todoSlice = createSlice({
+    name:'todo',
+    initialState,
+    reducers:{
+        addTodo:(state,action)=>{
+            const todo = {
+                id: nanoid(),
+                message: action.payload
+            }
+            state.todos.push(todo);
+            localStorage.setItem("todos",JSON.stringify(state.todos));
+        },
+        removeTodo:(state,action)=>{
+            state.todos = state.todos.filter((todo)=>todo.id !== action.payload);
+            localStorage.setItem("todos",JSON.stringify(state.todos));
+        }
+    }
+})
+
+export const {addTodo, removeTodo} = todoSlice.actions
+
+export default todoSlice.reducer
